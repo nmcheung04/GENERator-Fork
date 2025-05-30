395420353f595f86811201176b103f4da10d4a71
Nicholas Cheung <nicholas@Nicholass-MacBook-Air.local>
2025-05-30 10:44:21 -0400

    Added Prompt Module

--- src/tasks/downstream/models/vit_prompt/model_with_prompt.py
+++ src/tasks/downstream/models/vit_prompt/model_with_prompt.py
@@ -1,7 +1,16 @@
 from transformers import AutoModelForSequenceClassification
-from prompt_projector import PromptProjector
+# from prompt_projector import PromptProjector
 import torch
+import torch.nn as nn
 
+class PromptProjector(nn.Module):
+    def __init__(self, prompt_length: int = 1, hidden_size: int = 1024):
+        super().__init__()
+        self.prompt = nn.Parameter(torch.randn(prompt_length, hidden_size))
+
+    def forward(self, batch_size: int):
+        return self.prompt.unsqueeze(0).expand(batch_size, -1, -1)
+    
 class PromptedSeqClassification(AutoModelForSequenceClassification):
     def __init__(self, config, prompt_length = 1, prompt_dim = 1024):
         super().__init__(config)
